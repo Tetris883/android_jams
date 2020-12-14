c28f199dd54d57841ab3454efd70f5d751963cf1
Tetris883 <75837844+Tetris883@users.noreply.github.com>
2020-12-14 22:17:39 +0530

    Create q10.kt

---
+++ week_1/q10.kt
@@ -0,0 +1,28 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+fun main() {
+    
+    var h: Int
+    
+    h = 4
+	
+    
+    var n =0
+    
+    while(n<h)
+    {
+        var a = 0
+        
+        while(a<=n)
+        {
+            print("#")
+            a++
+        }
+        	
+        println()
+        n++
+    }
+}
