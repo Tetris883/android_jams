ddda84747341c178a3e0dfb47b6d8fe46bbb876a
Tetris883 <75837844+Tetris883@users.noreply.github.com>
2020-12-14 21:59:16 +0530

    Create q6.kt

---
+++ week_1/q6.kt
@@ -0,0 +1,17 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+fun main() {
+    
+    var c: Char
+    
+    c = 'f';
+    
+    if((c in 'A'..'Z') or (c in 'a'..'z'))
+    	println("Its an alphabet")
+        
+    else
+    	println("Its not an alphabet")
+}
